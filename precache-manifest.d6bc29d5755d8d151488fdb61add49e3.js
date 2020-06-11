3942837aa943d380876ccff297c582a36af3a17a
Dmitry Bolshakov <vanove2013@yandex.ru>
2020-06-11 11:07:31 +0300

    Updates

---
+++ precache-manifest.d6bc29d5755d8d151488fdb61add49e3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9980bbfd94b68db6f54612d211865eb1",
+    "url": "/social-material-app/index.html"
+  },
+  {
+    "revision": "753294ba3a1a24b4c3e1",
+    "url": "/social-material-app/static/css/3.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a046cf5add1ecea4e9dc",
+    "url": "/social-material-app/static/css/4.659f9a1a.chunk.css"
+  },
+  {
+    "revision": "0da088a5bc28b1f108a0",
+    "url": "/social-material-app/static/css/5.2b62b5d1.chunk.css"
+  },
+  {
+    "revision": "507875c635a9e74b77a5",
+    "url": "/social-material-app/static/css/6.aeddb040.chunk.css"
+  },
+  {
+    "revision": "9203a46e31a6100def0f",
+    "url": "/social-material-app/static/css/7.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1f55e59c91dc58a4b9d7",
+    "url": "/social-material-app/static/css/main.2fede2bc.chunk.css"
+  },
+  {
+    "revision": "a27deda0cfbc47d55a97",
+    "url": "/social-material-app/static/js/0.2f8bb0c2.chunk.js"
+  },
+  {
+    "revision": "753294ba3a1a24b4c3e1",
+    "url": "/social-material-app/static/js/3.2b6f8e05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-material-app/static/js/3.2b6f8e05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a046cf5add1ecea4e9dc",
+    "url": "/social-material-app/static/js/4.2be1fe4d.chunk.js"
+  },
+  {
+    "revision": "0da088a5bc28b1f108a0",
+    "url": "/social-material-app/static/js/5.2af70583.chunk.js"
+  },
+  {
+    "revision": "507875c635a9e74b77a5",
+    "url": "/social-material-app/static/js/6.738a6a65.chunk.js"
+  },
+  {
+    "revision": "9203a46e31a6100def0f",
+    "url": "/social-material-app/static/js/7.b151f364.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/social-material-app/static/js/7.b151f364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f55e59c91dc58a4b9d7",
+    "url": "/social-material-app/static/js/main.16dcc881.chunk.js"
+  },
+  {
+    "revision": "74557b5144a34d91d018",
+    "url": "/social-material-app/static/js/runtime-main.8d3219fc.js"
+  },
+  {
+    "revision": "8931c0d830d1dfc0fb8a7bd8cdfeeb6e",
+    "url": "/social-material-app/static/media/default-user.8931c0d8.jpg"
+  },
+  {
+    "revision": "078ba7094ef0c9571e15f518bdbca620",
+    "url": "/social-material-app/static/media/preloader.078ba709.svg"
+  }
+]);
